51694029f5abbbbfb904bab104f48486d09ce4ff
Nathan Charles <nathan@nathancharles.com>
2020-01-23 17:17:49 -0600

    Update location.component.js

--- src/pages/meetup/components/location/location.component.js
+++ src/pages/meetup/components/location/location.component.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card } from '@material-ui/core';
+import { Card, CardHeader } from '@material-ui/core';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 import GoogleMapReact from 'google-map-react';
 import PropTypes from 'prop-types';
 
@@ -8,12 +9,12 @@ import useStyles from './location.styles';
 const Location = ({ venue }) => {
   const classes = useStyles({});
 
-  const { lat, lon: lng } = venue;
-
+  const { name, address_1: address, city, state, lat, lon: lng } = venue;
   const center = { lat, lng };
 
   return (
     <Card className={classes.card}>
+      <CardHeader avatar={<LocationOnIcon />} title={name} subheader={`${address} - ${city}, ${state}`} />
       <GoogleMapReact
         bootstrapURLKeys={{ key: 'AIzaSyDnJLojru65uF52ykcv9w_-5Zr03INZgZU' }}
         defaultCenter={center}
